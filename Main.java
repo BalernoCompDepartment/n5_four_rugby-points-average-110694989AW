c265990bd7776f0e07158d75e2a5478b9f2a5bbe
110694989AW <110694989@ea.edin.sch.uk>
2021-01-28 10:25:49 +0000

    Wrote the program - it wouldn’t let me round the number so I did it as a real

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String name = Keyboard.getText("what is your name?");
+    String team = Keyboard.getText("what is your team?");
+    int total = 0;
+    
+    for(int games = 0; games < 5; games++) {
+      total = total + Keyboard.getInt("what did you get in game " + (games + 1) + "?");
+    }
+    
+    double average = total/5;
+    
+    String output = name + " for " + team + " scored an average of " + average + " per game";
+    
+    Screen.display(output, "player average");
   }
 }
